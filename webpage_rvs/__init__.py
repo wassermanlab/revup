7fa48e21f1645b77c1a5005ee029705cff16adab
brittanyhewitson <britt.hewitson@gmail.com>
2021-05-27 12:55:28 -0700

    updated setup

--- webpage_rvs/__init__.py
+++ webpage_rvs/__init__.py
@@ -4,11 +4,15 @@ import sys
 from flask import Flask
 from flask_cors import CORS
 from flask_dynamo import Dynamo
+from dotenv import load_dotenv, find_dotenv
 
+# Load the environment variables
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(find_dotenv())
 
 app = Flask(__name__, static_folder='./app/build', static_url_path='/')
 app.config.from_mapping(
-    SECRET_KEY='dev'
+    SECRET_KEY=os.environ.get("SECRET_KEY")
 )
 # TODO: Add this to a config file
 app.config['DYNAMO_TABLES'] = [
@@ -35,7 +39,7 @@ except OSError:
     pass
 
 cors = CORS(app, resources={
-    r"/*": {"origins": ["http://localhost:3000", "https://regulatory-variants.netlify.app"]}
+    r"/*": {"origins": ["http://localhost:3000", "https://regulatory-variants.netlify.app", "http://ec2-44-231-133-239.us-west-2.compute.amazonaws.com"]}
 })
 
 import webpage_rvs.src.routes
